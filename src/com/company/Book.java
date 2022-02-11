a38f91efdd76489c7a8684264d7e779380390407
mrdm026M <mrdm026@gmail.com>
2022-02-11 09:37:48 +0530

    Changed Data Structure

---
+++ src/com/company/Book.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.*;
+
+public class Book {
+    private int isbn;
+    private String title;
+    Set<String> author = new HashSet<>();
+    Set<String> keyword = new HashSet<>();
+}
