3e6cb33a1d93bdb5a4df5eea5cd6ae8cca1bd727
mrdm026M <mrdm026@gmail.com>
2022-02-11 09:38:50 +0530

    Removed code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,9 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World - This is Java Case Study - Book Sharing App");
-
-        Map<Integer, Set<String>> bookMap = new HashMap<>();
-        Users user = new Users("Dhruv","dhruv@gmail.com");
-
-        String[] bookAuthor = {"Author 1", "Author 2"};
-        String[] keywords = {"Keyword 1", "Keyword 2"};
-
-        Books book = new Books(101, "Java Learning", bookAuthor, keywords, user);
-        book.add();
-        book.display();
-        System.out.println("=================");
-        book.addBook();
-
-        bookMap = book.getBook();
-
-        for(Map.Entry m: bookMap.entrySet()){
-            System.out.println(m.getKey()+" "+m.getValue());
-        }
-
-        book.search(101,"Java");
+        System.out.println("Hello World");
     }
 }
