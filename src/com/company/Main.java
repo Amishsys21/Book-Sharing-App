182550f97b7d2abbf02879dff8b26c2eaec8a667
mrdm026M <mrdm026@gmail.com>
2022-02-11 11:55:39 +0530

    create book obj

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Book book = new Book(123456, "Java Programming Book");
+        book.setAuthor();
+        book.setKeyword();
+        book.display();
     }
 }
