756334f23f015c0e6c88a52a4edebddc5b42260b
mrdm026M <mrdm026@gmail.com>
2022-02-11 09:37:27 +0530

    Renamed file

---
+++ src/com/company/User.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class User {
+    public String userName;
+    public String userEmail;
+
+    public User(String userName, String userEmail) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+}
