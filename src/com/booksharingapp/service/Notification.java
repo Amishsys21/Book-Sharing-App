32d9821b79f3c863ced4ceeeb60e5e6b271a4433
mrdm026M <mrdm026@gmail.com>
2022-02-15 17:19:44 +0530

    Create Notification class

---
+++ src/com/booksharingapp/service/Notification.java
@@ -0,0 +1,9 @@
+package com.booksharingapp.service;
+
+public class Notification {
+    private String from;
+    private String to;
+    private String subject;
+    private String message;
+    WaitingQueue wq;
+}
