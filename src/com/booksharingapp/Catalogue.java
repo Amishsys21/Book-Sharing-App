5c56f557b4774269fb2588b7cbf60596e7cd79d2
mrdm026M <mrdm026@gmail.com>
2022-02-16 16:00:33 +0530

    Add borrower class, search class & checkstatus class objects & updated old code

--- src/com/booksharingapp/Catalogue.java
+++ src/com/booksharingapp/Catalogue.java
@@ -1,6 +1,9 @@
 package com.booksharingapp;
 
-import com.booksharingapp.model.Owner;
+import com.booksharingapp.model.Borrower;
+import com.booksharingapp.model.User;
+import com.booksharingapp.service.CheckStatus;
+import com.booksharingapp.service.Search;
 import com.booksharingapp.service.WaitingQueue;
 
 import java.util.*;
@@ -10,137 +13,145 @@ import java.util.logging.Logger;
 public class Catalogue {
     private static Logger logger = Logger.getLogger(Catalogue.class.getName());
     private static Map<Long, Book> shelves = new HashMap<>();
-    private static Map<String, Queue<Owner>> borrowers = new HashMap<>();
+    private static Map<String, Queue<User>> waitingQueueUser = new HashMap<>();
+    private static Map<String, Borrower> rentedBookUser = new HashMap<>();
 
     public static void main(String[] args) {
         // add book
         Book book = new Book(123456, "Java Programming Book");
-        Owner owner = new Owner("Dhruv", "dhruv@gmail.com");
+        User user = new User("Dhruv", "dhruv@gmail.com");
         book.addAuthor("JavaBoy");
         book.addAuthor("JavaDog");
         book.addKeyword("Java");
         book.addKeyword("Programming");
-        book.addOwner(owner);
+        book.addOwner(user);
         shelves.put(book.getIsbn(), book);
 
         book = new Book(323232, "Java Book");
-        owner = new Owner("Harshit", "harshit@gmail.com");
+        user = new User("Harshit", "harshit@gmail.com");
         book.addAuthor("JavaBoy");
         book.addAuthor("JavaDog");
         book.addKeyword("Java");
         book.addKeyword("Programming");
-        book.addOwner(owner);
+        book.addOwner(user);
+        book.setStatus("Rented");
         shelves.put(book.getIsbn(), book);
+        Borrower borrower = new Borrower("Catty", "catty@gmail.com");
+        borrower.rentBook(book.getIsbn(), book);
+        rentedBookUser.put(borrower.getName(), borrower);
 
         book = new Book(876538, "Python Programming Book");
-        owner = new Owner("Mayank", "mayank@gmail.com");
+        user = new User("Mayank", "mayank@gmail.com");
         book.addAuthor("PythonBoy");
         book.addAuthor("PythonSnake");
         book.addKeyword("Python");
         book.addKeyword("Programming");
         book.addKeyword("Book");
-        book.addOwner(owner);
-
+        book.addOwner(user);
+        book.setStatus("Rented");
         shelves.put(book.getIsbn(), book);
+        borrower.rentBook(book.getIsbn(), book);
+        rentedBookUser.put(borrower.getName(), borrower);
 
         book = new Book(1432432, "C++ Programming Book");
-        owner = new Owner("Kartik", "kartik@gmail.com");
+        user = new User("Kartik", "kartik@gmail.com");
         book.addAuthor("C++Boy");
         book.addAuthor("C++Cat");
         book.addKeyword("Cat");
         book.addKeyword("Programming");
         book.addKeyword("Book");
-        book.addOwner(owner);
-
+        book.addOwner(user);
+        book.setStatus("Rented");
         shelves.put(book.getIsbn(), book);
+        borrower = new Borrower("Mathew", "Mathew@gmail.com");
+        borrower.rentBook(book.getIsbn(), book);
+        rentedBookUser.put(borrower.getName(), borrower);
 
-        shelves.forEach((key, value) -> logger.log(Level.INFO,() -> key + " " + value.getTitle()));
+        // display all books
+        displayAllBooks(shelves);
+
+        // display all rented books
+        displayAllRentedBook(rentedBookUser);
 
         // search book
-        Map<Long, List<String>> searchTitleResult = searchTitle("Java Book", shelves);
-        displayResult(searchTitleResult);
+        Search search = new Search();
 
-        Map<Long, List<String>> searchAuthorResult = searchAuthor("C++Boy", shelves);
-        displayResult(searchAuthorResult);
+        // search by title
+        search.searchByTitle("Java", shelves);
+        search.display();
 
-        Map<Long, List<String>> searchKeywordResult = searchKeyword("Book", shelves);
-        displayResult(searchKeywordResult);
+        // search by author
+        search.searchByAuthor("PythonBoy", shelves);
+        search.display();
+
+        // search by keyword
+        search.searchByKeyword("Book", shelves);
+        search.display();
 
         // waiting queue
-        WaitingQueue waitingQueue = new WaitingQueue("Java Programming Book");
-        waitingQueue.addBorrower(owner);
-        owner = new Owner("Dan", "dan@gmail.com");
-        waitingQueue.addBorrower(owner);
-
-        logger.log(Level.INFO, "Total People in the queue: " + waitingQueue.getQueueSize());
-        borrowers.put(waitingQueue.getTitle(), waitingQueue.getWaitingQueue());
+        WaitingQueue waitingQueue = new WaitingQueue("Java Book");
+        waitingQueue.addUserToQueue(user);
+        user = new User("Dan", "dan@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        waitingQueueUser.put(waitingQueue.getTitle(), waitingQueue.getWaitQueue());
 
 
         waitingQueue = new WaitingQueue("Python Programming Book");
-        owner = new Owner("Gill", "gill@gmail.com");
-        waitingQueue.addBorrower(owner);
-        owner = new Owner("chill", "chill@gmail.com");
-        waitingQueue.addBorrower(owner);
-        waitingQueue.displayQueue();
-        logger.log(Level.INFO, "Total People in the queue: " + waitingQueue.getQueueSize());
-        borrowers.put(waitingQueue.getTitle(), waitingQueue.getWaitingQueue());
+        user = new User("Gill", "gill@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        user = new User("chill", "chill@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        waitingQueueUser.put(waitingQueue.getTitle(), waitingQueue.getWaitQueue());
+
+        waitingQueue = new WaitingQueue("C++ Programming Book");
+        user = new User("May", "may@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        user = new User("Amy", "amy@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        user = new User("Yam", "yam@gmail.com");
+        waitingQueue.addUserToQueue(user);
+        waitingQueueUser.put(waitingQueue.getTitle(), waitingQueue.getWaitQueue());
 
         // display borrowers list
-        borrowers.forEach((key, value) -> logger.log(Level.INFO, () -> key + " " + value.element().getName()));
+        displayBorrowersList(waitingQueueUser);
+
+        // to check status of particular book
+        CheckStatus cs = new CheckStatus("C++ Programming Book");
+        cs.checkStatus(shelves);
+        cs.statusOption(rentedBookUser, waitingQueueUser);
     }
 
-    private static void displayResult(Map<Long, List<String>> searchResult) {
-        searchResult.forEach((key, value) -> logger.log(Level.INFO,() -> key + ":" + value));
+    // display function -> to display all books
+    private static void displayAllBooks(Map<Long, Book> shelves) {
+        shelves.forEach((key, value) -> {
+            logger.log(Level.INFO, () -> "Book ISBN: " + key);
+            logger.log(Level.INFO, () -> "Book Name: " + value.getTitle());
+            logger.log(Level.INFO, () -> "Book Authors: " + value.getAuthors());
+            value.getOwner().forEach(val -> {
+                logger.log(Level.INFO, () -> "Book Owner's Name: " + val.getName());
+                logger.log(Level.INFO, () -> "Book Owner's Email: " + val.getEmail());
+            });
+            logger.log(Level.INFO, () -> "Status: " + value.getStatus());
+            logger.log(Level.INFO, "================================");
+        });
     }
 
-    private static Map<Long, List<String>> searchTitle(String title, Map<Long, Book> shelves) {
-        logger.log(Level.INFO, "============ Map ==============");
-        Map<Long, List<String>> mapResult = new HashMap<>();
-        List<String> result = new ArrayList<>();
-        for (Map.Entry<Long, Book> entry: shelves.entrySet()) {
-            if (entry.getValue().getTitle().equalsIgnoreCase(title)) {
-                result.add(String.valueOf(entry.getKey()));
-                result.add(entry.getValue().getTitle());
-                result.add(entry.getValue().getAuthors().toString());
-                result.add(entry.getValue().getKeywords().toString());
-                mapResult.put(entry.getKey(), result);
-                return mapResult;
-            }
-        }
-        return Collections.emptyMap();
+    // display function -> to display all rented books
+    private static void displayAllRentedBook(Map<String, Borrower> rentedBookUser) {
+        rentedBookUser.forEach((key, value) -> {
+            logger.log(Level.INFO, key);
+            logger.log(Level.INFO, () -> value.getName() + ", " + value.getEmail());
+            value.displayAllBooks();
+        });
     }
 
-    private static Map<Long, List<String>> searchAuthor(String author, Map<Long, Book> shelves) {
-        logger.log(Level.INFO, "============ Map ==============");
-        Map<Long, List<String>> mapResult = new HashMap<>();
-        List<String> result = new ArrayList<>();
-        for (Map.Entry<Long, Book> entry: shelves.entrySet()) {
-            if (entry.getValue().getAuthors().contains(author)) {
-                result.add(String.valueOf(entry.getKey()));
-                result.add(entry.getValue().getTitle());
-                result.add(entry.getValue().getAuthors().toString());
-                result.add(entry.getValue().getKeywords().toString());
-                mapResult.put(entry.getKey(), result);
-                return mapResult;
-            }
-        }
-        return Collections.emptyMap();
-    }
-
-    private static Map<Long, List<String>> searchKeyword(String keyword, Map<Long, Book> shelves) {
-        logger.log(Level.INFO, "============ Map ==============");
-        Map<Long, List<String>> mapResult = new HashMap<>();
-        List<String> result = new ArrayList<>();
-        for (Map.Entry<Long, Book> entry: shelves.entrySet()) {
-            if (entry.getValue().getKeywords().contains(keyword)) {
-                result.add(String.valueOf(entry.getKey()));
-                result.add(entry.getValue().getTitle());
-                result.add(entry.getValue().getAuthors().toString());
-                result.add(entry.getValue().getKeywords().toString());
-                mapResult.put(entry.getKey(), result);
-                return mapResult;
-            }
-        }
-        return Collections.emptyMap();
+    // display function -> to display borrowers list
+    private static void displayBorrowersList(Map<String, Queue<User>> waitingQueueUser) {
+        waitingQueueUser.forEach((key, value) -> {
+            logger.log(Level.INFO, key);
+            logger.log(Level.INFO, "Name - Email");
+            value.forEach(val -> logger.log(Level.INFO, () -> val.getName() + " - " + val.getEmail()));
+            logger.log(Level.INFO, "==========================");
+        });
     }
 }
