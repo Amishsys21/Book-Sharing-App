98919ea283e7c09f3a27da13cb51ea6bc007eeb1
mrdm026M <mrdm026@gmail.com>
2022-02-16 15:58:38 +0530

    Create class for already rented books

---
+++ src/com/booksharingapp/model/Borrower.java
@@ -0,0 +1,62 @@
+package com.booksharingapp.model;
+
+import com.booksharingapp.Book;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Borrower {
+    private static Logger logger = Logger.getLogger(Borrower.class.getName());
+    private String name;
+    private String email;
+    private Map<Long, Book> rentedBooks;
+
+    public Borrower(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.rentedBooks = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // function to rent book
+    public void rentBook(Long isbn, Book book) {
+        rentedBooks.put(isbn, book);
+    }
+
+    // function to display all the rented books
+    public void displayAllBooks() {
+        rentedBooks.forEach((key, value) -> {
+            logger.log(Level.INFO, ()-> "Book ISBN: " + key);
+            logger.log(Level.INFO,() -> "Book Name: " + value.getTitle());
+            logger.log(Level.INFO,() -> "Book Authors: " + value.getAuthors());
+            value.getOwner().forEach(val -> {
+                logger.log(Level.INFO,() -> "Book Owner's Name: " + val.getName());
+                logger.log(Level.INFO,() -> "Book Owner's Email: " + val.getEmail());
+            });
+            logger.log(Level.INFO,() -> "Book Status: " + value.getStatus());
+        });
+    }
+
+    // to return the map of rented book
+    public Map<Long, Book> getRentedBooks() {
+        return rentedBooks;
+    }
+
+    // function to return book
+    public void returnBook(String title) {
+        for (Map.Entry<Long, Book> entry: rentedBooks.entrySet()) {
+            if (entry.getValue().getTitle().equalsIgnoreCase(title)) {
+                rentedBooks.remove(entry.getKey());
+            }
+        }
+    }
+}
