348b371f02bf72beb6ac909c446518a83516bf78
mrdm026M <mrdm026@gmail.com>
2022-02-18 16:43:16 +0530

    remove static from logger variable

--- src/com/booksharingapp/service/Search.java
+++ src/com/booksharingapp/service/Search.java
@@ -8,7 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Search {
-    private static Logger logger = Logger.getLogger(Search.class.getName());
+    private Logger logger = Logger.getLogger(Search.class.getName());
     private Map<Long, Book> searchResult;
 
     public Search() {
